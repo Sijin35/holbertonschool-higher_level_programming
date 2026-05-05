3fe2a6bac1e52873788b4b13678833901b59a30e
Sijin35 <sijinsingh222@gmail.com>
2026-05-05 08:23:32 +0000

    Changed string slicing to modulo division followed by -ve check to accomodate -ve numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,14 @@
 import random
 
 number = random.randint(-10000, 10000)
-last = int(str(number)[-1]) * (-1 if number < 0 else 1) # ternary operator
+last = number % 10
 
-if last < 6 and last != 0:
-    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
-elif last > 5:
+if number < 0 and last != 0:
+    last -=10
+
+if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
-else:
+elif last == 0:
     print(f"Last digit of {number} is {last} and is 0")
+else:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
