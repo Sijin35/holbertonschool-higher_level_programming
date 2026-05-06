1cfc8221473a32029d23242fbc458f141f8f6a71
Sijin35 <sijinsingh222@gmail.com>
2026-05-06 10:30:12 +0000

    Added new line at the end

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    if i > 0:
-        print(", ", end="")
-    print("{:02d}".format(i), end="")
-    
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
