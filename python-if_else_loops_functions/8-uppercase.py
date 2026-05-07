00c42686731b0b8df997bc642ac5be6c6dad9ab9
Sijin35 <sijinsingh222@gmail.com>
2026-05-07 00:31:34 +0000

    Added missing whitespace aroung operato

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     result = ""
     for i in str:
         if 'a' <= i <= 'z':
-            result += chr(ord(i) -32)
+            result += chr(ord(i) - 32)
         else:
             result += i
     print("{}".format(result))
