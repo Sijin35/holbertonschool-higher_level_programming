723af137eabed819b65539f5d259d03b85828ae6
Sijin35 <sijinsingh222@gmail.com>
2026-05-07 05:27:21 +0000

    Created an empty array, and used .append() and .join() to style output as required

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,15 @@
 
 
 def fizzbuzz():
+    myArray = []
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            myArray.append("FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 ==0:
-            print("Buzz")
+            myArray.append("Fizz")
+        elif i % 5 == 0:
+            myArray.append("Buzz")
         else:
-            print(f"{i}", end=" ")
+            myArray.append(i)
+    result = " ".join(map(str, myArray))
+    print(result)
