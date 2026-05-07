93c10732f4c8dc2c4e02a0b912e90077874baddb
Sijin35 <sijinsingh222@gmail.com>
2026-05-07 06:47:07 +0000

    Removed second occurrence of add_0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
 
-    print("{}".format(a), " + ", "{}".format(b), " = ", "{}".format(add_0.add(a,b)))
+    print("{}".format(a), " + ", "{}".format(b), " = ", "{}".format(add(a,b)))
