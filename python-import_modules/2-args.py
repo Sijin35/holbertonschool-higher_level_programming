ec7bc7cf3c10df29121631e0c8dab3a9fd29f78b
Sijin35 <sijinsingh222@gmail.com>
2026-05-08 10:08:04 +0000

    Fixed output when there are 0 arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     elif a > 1:
         print("{}".format(len(argv) - 1), "arguments:")
     elif a == 0:
-        print("{}".format(len(argv) - 1), "argument.")
+        print("{}".format(len(argv) - 1), "arguments.")
 
     for i in range(1, a + 1):
         print("{}: {}".format(i, argv[i]))
