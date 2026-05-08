ec851eb6880101e3d6b1a2bb8697a5f9a0615246
Sijin35 <sijinsingh222@gmail.com>
2026-05-08 10:07:15 +0000

    Removed excess : to fix output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     if a == 1:
         print("{}".format(len(argv) - 1), "argument:")
     elif a > 1:
-        print("{}:".format(len(argv) - 1), "arguments:")
+        print("{}".format(len(argv) - 1), "arguments:")
     elif a == 0:
-        print("{}:".format(len(argv) - 1), "argument.")
+        print("{}".format(len(argv) - 1), "argument.")
 
     for i in range(1, a + 1):
         print("{}: {}".format(i, argv[i]))
