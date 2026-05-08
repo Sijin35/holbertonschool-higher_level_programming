48e65e571eab3c22b1db43d08b0be842cb85d660
Sijin35 <sijinsingh222@gmail.com>
2026-05-08 10:04:54 +0000

    Fixed placement of : as per given example

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     a = len(argv) - 1
     
     if a == 1:
-        print("{}:".format(len(argv) - 1), "argument")
+        print("{}".format(len(argv) - 1), "argument:")
     elif a > 1:
-        print("{}:".format(len(argv) - 1), "arguments")
+        print("{}:".format(len(argv) - 1), "arguments:")
     elif a == 0:
         print("{}:".format(len(argv) - 1), "argument.")
 
