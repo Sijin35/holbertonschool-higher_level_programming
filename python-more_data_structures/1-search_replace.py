d30ffc8b00b64fe46d4c5f3b20fee03f7f3a4ee2
Sijin35 <sijinsingh222@gmail.com>
2026-05-14 01:26:45 +0000

    Removed trailing whitespace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -6,5 +6,5 @@ def search_replace(my_list, search, replace):
         if v == search:
             new[i] = replace
     return new
-# new = [replace if i == search else i for i in my_list] 
+# new = [replace if i == search else i for i in my_list]
 # List comprehension if want to use
