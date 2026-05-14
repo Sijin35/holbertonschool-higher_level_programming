92972910c3e83a3eb8419d4461c17c92ee2eb521
Sijin35 <sijinsingh222@gmail.com>
2026-05-14 01:25:36 +0000

    Fixed spaces for pycodestyle hopefully

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ def search_replace(my_list, search, replace):
     new = my_list[:]
     for i, v in enumerate(my_list):
         if v == search:
-           new[i] = replace 
+            new[i] = replace
     return new
-
-    #new = [replace if i == search else i for i in my_list] List comprehension if want to use
+# new = [replace if i == search else i for i in my_list] 
+# List comprehension if want to use
