a60b3ec62df456704e5f7a5f968f6b375de87cce
Sijin35 <sijinsingh222@gmail.com>
2026-05-15 11:09:08 +0000

    Added whitespace after keyword

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -9,4 +9,4 @@ def update_dictionary(a_dictionary, key, value):
             a_dictionary[i] = value
         else:
             a_dictionary.update({key: value})
-    return(a_dictionary)
+    return (a_dictionary)
