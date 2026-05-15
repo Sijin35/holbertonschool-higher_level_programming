4829523b209bd46de88922649b8db84b1201298e
Sijin35 <sijinsingh222@gmail.com>
2026-05-15 11:22:45 +0000

    Used pythonic way to write the code

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary == {}:
-        a_dictionary.update({key: value})
-    li = list(a_dictionary.keys())
-    for i in li:
-        if i == key:
-            a_dictionary[i] = value
-        else:
-            a_dictionary.update({key: value})
+   # Pythonic coding apparently
+    a_dictionary[key] = value
     return (a_dictionary)
+
+    # C way apparently
+    # if a_dictionary == {}:
+   #     a_dictionary.update({key: value})
+
+   # li = list(a_dictionary.keys())
+
+   # for i in li:
+   #     if i == key:
+   #         a_dictionary[i] = value
+   #     else:
+   #         a_dictionary.update({key: value})
+   # return (a_dictionary)
