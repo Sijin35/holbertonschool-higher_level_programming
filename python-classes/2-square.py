9df1f9467c5b4f684d1072769e15e5d2c792c5cc
Sijin35 <sijinsingh222@gmail.com>
2026-05-18 12:21:54 +0000

    Updated documentation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
-"""Defines a square with private attribute size and type/value verification"""
+"""Defines a square module."""
 
 class Square:
+    """Represents a square with private attribute size and type/value verification."""
 
-    """Represents a square with private attribute size and type/value verification"""
     def __init__(self, size=0):
-
-        """Initializes a square with private attribute size and type/value verification"""
+        """Initializes a square with type/value verification"""
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
