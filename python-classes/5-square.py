da4327a116d4548df33750ced8379b1f8e4e4371
Sijin35 <sijinsingh222@gmail.com>
2026-05-18 13:30:19 +0000

    Created a class square that has getter and setter for retrieval and setting respectively, returns the current area and prints the square with the character #

---
+++ python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines a square module."""
+
+class Square:
+    """Represents a square with private attribute size and type/value verification
+    which returns the current square area and prints the square with character #
+    in stdout.
+    """
+
+    def __init__(self, size=0):
+        """Initializes a square with attribute size which will be made private
+        using the setter."""
+
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the current area of the square."""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the squre with type/value verification."""
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints in stdout the square with the character #."""
+
+        for i in range(1, self.__size):
+            print("#" * self.__size)
