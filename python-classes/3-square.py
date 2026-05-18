9e9deb8670ead279cb0131a6fdf9070119677132
Sijin35 <sijinsingh222@gmail.com>
2026-05-18 12:39:24 +0000

    Created a class square with private attribute size and type/value verification which returns current area of square

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a square module."""
+
+class Square:
+    """Represents a square with private attribute size and public method area."""
+
+    def __init__(self, size=0):
+        """Initializes a square with type/value verification"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Returns the current area of square."""
+
+        return self.__size * self.__size
