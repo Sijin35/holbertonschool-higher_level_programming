dd6ef9ac5fac89b34cc7125cf73ec64f732d17ac
Sijin35 <sijinsingh222@gmail.com>
2026-05-18 12:01:31 +0000

    Made documenation more descriptive

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 class Square:
-    """Defines a square."""
+    """Defines a square with private attribute size."""
 
     def __init__(self, size=0):
-        """Initializes a square."""
+        """Initializes a square with private attribute size."""
         self.__size = size
