e589caa2ac4b42c8c07c2e34a67d8af70ed784b4
Sijin35 <sijinsingh222@gmail.com>
2026-05-19 02:17:47 +0000

    Comment layout

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
-"""Defines a square module."""
+"""
+
+Defines a square module.
+"""
 
 
 class Square:
-    """Represents a square with private attribute size and type/value verification
+    """
+
+    Represents a square with private attribute size and type/value verification
     which returns the current square area.
     """
 
     def __init__(self, size=0):
-        """Initializes a square with attribute size which will be made private
-        using the setter"""
+        """
+
+        Initializes a square with attribute size which will be made private
+        using the setter
+        """
 
         self.size = size
 
     @property
     def size(self):
-        """Retrieves the size of the square."""
+        """
+        
+        Retrieves the size of the square.
+        """
 
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square with type/value verification"""
+        """
+
+        Sets the size of the square with type/value verification
+        """
 
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -30,6 +44,9 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Returns the current square area"""
+        """
+
+        Returns the current square area
+        """
 
         return self.__size * self.__size
