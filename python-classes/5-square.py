9455687bbe60d9894ea77db608261599375cf24b
Sijin35 <sijinsingh222@gmail.com>
2026-05-19 02:22:25 +0000

    Spacing fix

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-"""Defines a square module."""
+"""
+
+Defines a square module.
+"""
 
 
 class Square:
-    """Represents a square with size validation and printing ability."""
+    """
+
+    Represents a square with size validation and printing ability.
+    """
 
     def __init__(self, size=0):
-        """Initializes a square with attribute size which will be made private
-        using the setter."""
+        """
+
+        Initializes a square with attribute size which will be made private
+        using the setter.
+        """
 
         self.size = size
 
     @property
     def size(self):
-        """Retrieves the current area of the square."""
+        """
+
+        Retrieves the current area of the square.
+        """
 
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size of the squre with type/value verification."""
+        """
+
+        Sets the size of the squre with type/value verification.
+        """
 
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -28,12 +43,18 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Returns the current square area."""
+        """
+
+        Returns the current square area.
+        """
 
         return self.__size * self.__size
 
     def my_print(self):
-        """Prints in stdout the square with the character #."""
+        """
+
+        Prints in stdout the square with the character #.
+        """
 
         if self.__size == 0:
             print()
