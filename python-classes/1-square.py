c67e90d82adec49eed907977932c45f71b1b9bbf
Sijin35 <sijinsingh222@gmail.com>
2026-05-19 02:13:26 +0000

    Comment layout

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""Defines a square with private attribute size."""
+"""
+
+Defines a square with private attribute size.
+"""
 
 
 class Square:
-    """Represents a square with private attribute size."""
-    
+    """
+
+    Represents a square with private attribute size.
+    """
     def __init__(self, size=0):
-        """Initializes a square with private attribute size."""
-        
+        """
+
+        Initializes a square with private attribute size.
+        """
+
         self.__size = size
