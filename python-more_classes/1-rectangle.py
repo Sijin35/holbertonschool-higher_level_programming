eb0a30c2131958b741ba757c3e3deb3b432386f5
Sijin35 <sijinsingh222@gmail.com>
2026-05-19 04:24:55 +0000

    Fixed error message

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
     def width(self, value):
 
         if not isinstance(value, int):
-            raise TypeError("width must be integer")
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -39,7 +39,7 @@ class Rectangle:
     def height(self, value):
 
         if not isinstance(value, int):
-            raise TypeError("height must be integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
