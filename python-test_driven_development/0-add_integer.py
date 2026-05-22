e89582a49788f30c795efbbe73a6f4ae7fa26b6e
Sijin35 <sijinsingh222@gmail.com>
2026-05-22 13:01:34 +0000

    Function that adds 2 integers (need to be tested)

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+
+Module that adds 2 integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+
+    Function that retruns the addition of two integers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
