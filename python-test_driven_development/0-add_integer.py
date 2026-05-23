3382503bcdd9632d180aa67004b8ad3ecaf21818
Sijin35 <sijinsingh222@gmail.com>
2026-05-23 10:59:53 +0000

    Removed addidional if checks for float type

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,10 +12,8 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+    res_1 = int(a)
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return a + b
+    res_2 = int(b)
+    return res_1 + res_2
