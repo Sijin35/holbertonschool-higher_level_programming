bfa1ded6292129111c708ea53ce64b265a780ea5
Sijin35 <sijinsingh222@gmail.com>
2026-05-25 15:18:32 +0000

    Deleted file

--- 0-lookup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module that returns the list of attributes and methods of an object"""
-
-
-def lookup(obj):
-    """Function that returns the list of attributes and methods of obejct"""
-
-    return dir(obj)
