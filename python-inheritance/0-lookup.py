e027f4749e90007406b0cac308506f895ddc6e03
Sijin35 <sijinsingh222@gmail.com>
2026-05-25 15:18:07 +0000

    Moved file to correct directory

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that returns the list of attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Function that returns the list of attributes and methods of obejct"""
+
+    return dir(obj)
