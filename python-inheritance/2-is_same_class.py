4c709ffff02d97bc7df82fef4c571a203fc30f62
Sijin35 <sijinsingh222@gmail.com>
2026-05-27 10:29:04 +0000

    Function that checks if object is in stance of specific class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/use/bin/python3
+"""Module that checks if object is instance of class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true if obj is instanc of a_class"""
+
+    return isinstance(obj, a_class)
