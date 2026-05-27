8d28b64f643f28d14bcd20231f5a14d9cae7b296
Sijin35 <sijinsingh222@gmail.com>
2026-05-27 10:47:00 +0000

    Created a class BaseGeometry with public instance method area() that is not defined

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that creates a class with public instance method"""
+
+
+class BaseGeometry:
+    """Represents a class with a public instance method"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
