7dfb629aff633ef121c4ffd6604a2eeb9ec20f28
Sijin35 <sijinsingh222@gmail.com>
2026-05-27 11:24:57 +0000

    Class BaseGeometry with two publci intance methods

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that creates class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Represents class BaseGeometry with public instance methods"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        self.name = name
+        self.value = value
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
