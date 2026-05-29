bdb298886d493193b27b02ba626b04dec7e6be4d
Sijin35 <sijinsingh222@gmail.com>
2026-05-29 10:55:26 +0000

    Fixed logic in __next__

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -7,10 +7,14 @@ class CountedIterator:
     def __init__(self, obj):
         self.iterator = iter(obj)
         self.counter = 0
-
+    
+    def __iter__(self):
+        return self
+    
     def get_count(self):
         return self.counter
 
     def __next__(self):
+        value = next(self.iterator)
         self.counter += 1
-        return next(self.iterator)
+        return value
