82ea9120e2b3205d2c5b78a62c4fb143ecc7117e
Sijin35 <sijinsingh222@gmail.com>
2026-05-31 11:25:14 +0000

    Class Dragon that uses mixin to show inheritance

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that uses mixin to show inheritance"""
+
+
+class SwimMixin:
+
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin:
+
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+
+    def roar(self):
+        print("The dragon roars!")
