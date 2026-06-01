06a034cc90132493ac9963ece2a200a0746cdae2
Sijin <sijinsingh222@gmail.com>
2026-06-01 13:14:06 +1000

    Correct file opening and reading in read_file function
    Fix the file opening by using the filename parameter correctly and correct the read method.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
   """Function that reads file and prints in stdout."""
   
-  with open('filename', encoding="utf-8")as f:
-    read_file = f.read()
+  with open(filename, encoding="utf-8")as f:
+    read_file = f.read)
     print(read_file, end="")
