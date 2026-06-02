5175fae8b6f4702b7bbc04f5ab3602cddaabe4b5
Sijin35 <sijinsingh222@gmail.com>
2026-06-02 13:47:29 +0200

    Corrected mode for open

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 def load_from_json_file(filename):
     """Creates object from JSON file"""
 
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
