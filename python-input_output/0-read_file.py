ec1b567bb62c18025fbba4ade71a587747d9abe5
Sijin35 <sijinsingh222@gmail.com>
2026-06-02 12:27:56 +0200

    commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
   """Function that reads file and prints in stdout."""
   
-  with open('filename', encoding="utf-8")as f:
-    read_file = f.read()
-    print(read_file, end="")
+  with open(filename, encoding="utf-8") as f:
+    read = f.read()
+    print(read, end="")
