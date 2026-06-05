45cccea1c0c3502e607b182610ce35899f76e40d
Sijin35 <sijinsingh222@gmail.com>
2026-06-05 12:38:04 +0200

    Class Student that returns key value pair based on attribute provided

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module that defines a class Student"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiates an object"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of Student instance"""
+
+        if attrs == None:
+            return self.__dict__
+        else:
+            result = {}
+            for k in attrs:
+                if k in self.__dict__:
+                    result[k] = self.__dict__[k]
+            return result
