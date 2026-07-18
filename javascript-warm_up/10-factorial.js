d4d2bc035cf8fb22193eb0a91cb619665e7be8f3
Sijin35 <sijinsingh222@gmail.com>
2026-07-18 22:27:25 +1000

    Semistandard fix

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,15 +4,15 @@ const num = parseInt(process.argv[2]);
 let fac;
 
 if (Number.isNaN(num)) {
-    fac = 1;
+  fac = 1;
 }
 
-function factorial(num) {
-    if (num === 1) {
-        return 1;
-    }
-    
-    return num * factorial(num - 1);
+function factorial (num) {
+  if (num === 1) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
 }
 
 console.log(factorial(num));
