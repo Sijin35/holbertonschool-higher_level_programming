8a4ecaa20df7a9fbf24be44f1a2fc812d3589607
Sijin35 <sijinsingh222@gmail.com>
2026-07-18 20:32:35 +1000

    Converting to integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const num = Number(process.argv[2]);
+const num = parseInt(process.argv[2]);
 
 if (Number.isInteger(num)) {
   console.log(`My number: ${num}`);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
