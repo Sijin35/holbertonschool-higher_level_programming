568ed6cc1ddc9b945bedadee439a0ecc6f675ad9
Sijin35 <sijinsingh222@gmail.com>
2026-07-18 21:55:44 +1000

    Square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const c = parseInt(process.argv[2]);
+
+if (Number.isNaN(c)) {
+  console.log('Missing size');
+}
+
+for (let i = 0; i < c; i++) {
+  let row = '';
+  for (let j = 0; j < c; j++) {
+    row += 'X ';
+  }
+  console.log(row);
+}
