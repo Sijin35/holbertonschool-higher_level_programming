3c6cebe5ca18d5b28bd68a162407c4f6bc939cbe
Sijin35 <sijinsingh222@gmail.com>
2026-07-18 22:34:56 +1000

    Fixed factorial of NaN

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const num = parseInt(process.argv[2]);
-let fac;
-
-if (Number.isNaN(num)) {
-  fac = 1;
-}
 
 function factorial (num) {
+  if (Number.isNaN(num)) {
+    return 1;
+  }
+
   if (num === 1) {
     return 1;
   }
