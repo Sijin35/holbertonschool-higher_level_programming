cd6cdd3557edefa5bc7e0b00ca0ebee939e02102
Sijin35 <sijinsingh222@gmail.com>
2026-07-21 00:05:26 +1000

    Undid sort logic changes which was made due to misinterpreting the task

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ const num = process.argv.slice(2);
 if (num.length <= 1){
   console.log(0);
 } else {
-  num.sort((a, b) => a - b);
-  console.log(num);
+  num.sort((a, b) => b - a);
+  console.log(num[1]);
 }
