a24c1204133bba5ffab2ab25e794e727676153b2
Sijin35 <sijinsingh222@gmail.com>
2026-07-20 23:52:04 +1000

    Semistandard satisfaction and made file executable

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const num = process.argv.slice(2); 
+const num = process.argv.slice(2);
 
 if (num.length <= 2) {
-    console.log(0);
+  console.log(0);
 } else {
-    num.sort((a, b) => b - a);
-    console.log(num[1]);
+  num.sort((a, b) => b - a);
+  console.log(num[1]);
 }
