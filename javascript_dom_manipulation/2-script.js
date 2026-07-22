17a8ba21d6f266f1b79bf4a60a8bebe9b03f95a4
Sijin35 <sijinsingh222@gmail.com>
2026-07-22 11:25:25 +1000

    Addng red on click

---
+++ javascript_dom_manipulation/2-script.js
@@ -0,0 +1,7 @@
+const h = document.querySelector('header');
+const r = document.querySelector('#red_header');
+
+r.addEventListener('click', () => {
+    h.classList.add('red');
+    // h.classList.toggle('red');
+});
